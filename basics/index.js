f8dcedf828b7a0f301c13f237b2a066e7c13779d
Sido-A <sidonie.abe.uk@gmail.com>
2021-05-25 19:44:57 +0100

    commit basic vue practice

---
+++ basics/index.js
@@ -0,0 +1,83 @@
+// const app = new Vue({
+//     el: "#app",
+//     data: {
+//         message: "Hello Vue!!!",
+//     },
+// });
+
+// app.message = "I have changed data";
+
+// //
+// const app2 = new Vue({
+//     el: "#app-2",
+//     data: {
+//         messages: "You loaded this page on" + new Date().toLocaleString(),
+//     },
+// });
+
+// //
+// const app3 = new Vue({
+//     el: "#app-3",
+//     data: {
+//         seen: true,
+//     },
+// });
+
+// //
+// const app4 = new Vue({
+//     el: "#app-4",
+//     data: {
+//         todos: [
+//             { text: "Learn JS" },
+//             { text: "Learn HTML" },
+//             { text: "Learn CSS" },
+//             { text: "Learn SASS" },
+//             { text: "Learn Vue" },
+//         ],
+//     },
+// });
+
+// //
+// const app5 = new Vue({
+//     el: "#app-5",
+//     data: {
+//         message: "Hello Vue.js!",
+//     },
+
+//     methods: {
+//         reverseMessage: function() {
+//             this.message = this.message.split("").reverse().join("");
+//         },
+//     },
+// });
+
+// //
+// const app6 = new Vue({
+//     el: "#app-6",
+//     data: {
+//         message: "",
+//     },
+// });
+
+// //
+// Vue.component("todo-item", {
+//     props: ["todo"],
+//     template: "<li>{{todo.text}}</li>",
+// });
+
+// const app7 = new Vue({
+//     el: "#app-7",
+//     data: {
+//         groceryList: [
+//             { id: 0, text: "Veg" },
+//             { id: 1, text: "Meat" },
+//             { id: 2, text: "Fruits" },
+//             { id: 3, text: "Fish" },
+//         ],
+//     },
+// });
+
+// //
+// // const app8 = new Vue({
+// //     el: "#app-8",
+// // });
